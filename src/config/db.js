75b5f69b8a164573db3452e366592946c2079c35
Peterpce <peterpec2022@gmail.com>
2026-06-20 08:33:34 +0100

    next

--- src/config/db.js
+++ src/config/db.js
@@ -6,7 +6,14 @@ dotenv.config();
 const { Pool } = pg;
 
 const db = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false // Required for Render Postgres
+  }
 });
 
 export default db;
